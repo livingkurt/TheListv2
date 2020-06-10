95a960e83a710d35db32694d7aaabf8bddc5347f
Kurt LaVacque <lavacquek@icloud.com>
2020-06-09 22:15:17 -0500

    Centered the Model Button

--- client/src/components/UtilityComponents/ButtonSymbol/ButtonSymbol.js
+++ client/src/components/UtilityComponents/ButtonSymbol/ButtonSymbol.js
@@ -7,13 +7,13 @@ import './button_symbol.css'
 const ButtonSymbol = (props) => {
 
   return (
-    <div >
-      <button
-        style={props.styles}
-        id={props.id}
-        onClick={() => props.on_click_function(props.id, props.priority)}
-        className="button_symbol">{props.children}</button>
-    </div>
+    // <div >
+    <button
+      style={{ ...props.styles, margin: "auto" }}
+      id={props.id}
+      onClick={() => props.on_click_function(props.id, props.priority)}
+      className="button_symbol">{props.children}</button>
+    // </div>
   );
 }
 
--- client/src/components/UtilityComponents/EditorInput/EditorInput.js
+++ client/src/components/UtilityComponents/EditorInput/EditorInput.js
@@ -6,12 +6,23 @@ import './editor_input.css'
 
 const EditorInput = (props) => {
 
+  // "this.style.width = ((this.value.length + 1) * 8) + 'px';"
+
+  const adjust_input_size = (e) => {
+    console.log(e.target.value)
+    e.target.style.width = ((e.target.value.length + 1) * 8) + 'px'
+    // e.style.width = ((e.value.length + 1) * 8) + 'px'
+  }
+
+
   return (
     <div style={{ width: "100%" }}>
       <input
+        onKeyPress={e => adjust_input_size(e)}
         defaultValue={props.value}
-        onBlur={e => props.on_change_function(e)}
+        // onBlur={e => props.on_change_function(e)}
         className="input_editor"
+        size={e => e.target.value.length}
         style={props.styles}
         id={props.id}
         placeholder={props.placeholder}
